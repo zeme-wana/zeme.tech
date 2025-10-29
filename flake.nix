793fe8a70a330e7ae6bbf883631e1ea84bd61487
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2025-10-29 10:40:03 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "zeme.tech-devShell";
+          shellHook = ''
+            eval "$(starship init bash)"
+          '';
+          buildInputs = [
+            pkgs.git
+            pkgs.starship
+            pkgs.jekyll
+          ];
+        };
+      };
+    };
+}
